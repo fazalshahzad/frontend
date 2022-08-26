64b3550d2b66eb27b813e6b6db0306faccf32c2a
fazalshahzad <shancompany322@gmail.com>
2022-08-26 22:11:24 +0500

    c

--- src/app/main-module/main-module.module.ts
+++ src/app/main-module/main-module.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { MainModuleRoutingModule } from './main-module-routing.module';
 import { MainModuleComponent } from './main-module.component';
+import { HeaderComponent } from './main-components/header/header.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { ContactUsComponent } from './main-components/contact-us/contact-us.component';
+import { AboutUsComponent } from './main-components/about-us/about-us.component';
+import { ProductsComponent } from './main-components/products/products.component';
 
 
 @NgModule({
   declarations: [
-    MainModuleComponent
+    MainModuleComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,
