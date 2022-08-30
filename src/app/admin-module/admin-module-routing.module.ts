9484e96667d2f3e413e20242df2b5113bf6586c5
fazalshahzad <shancompany322@gmail.com>
2022-08-30 16:13:22 +0500

    c

--- src/app/admin-module/admin-module-routing.module.ts
+++ src/app/admin-module/admin-module-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { D0ashboardComponent } from './admin-components/d0ashboard/d0ashboard.component';
+import { ProductsComponent } from './admin-components/products/products.component';
 import { AdminModuleComponent } from './admin-module.component';
 
 const routes: Routes = [
   { path: '', component: AdminModuleComponent,
 children:[
-  {path:'',component:D0ashboardComponent}
+  {path:'',component:D0ashboardComponent},
+  {path:'dashboard',component:D0ashboardComponent},
+  {path:'product',component:ProductsComponent}
 ] }
 ];
 
